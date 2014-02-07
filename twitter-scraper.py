a4430bd8cdab3a8cc646670f0135adb11cfcbf71
Frank Bi <frank@frankbi.com>
2014-02-07 11:19:46 -0500

    asdf

--- twitter-scraper.py
+++ twitter-scraper.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
 
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
 import sys
+import csv
 
-r = requests.get("http://twitter.com/" + sys.argv[1])
-soup = BeautifulSoup(r.text)
-followers = soup.findAll("a", {"data-element-term":"follower_stats"})
-print "Followers: " + followers[0].find("strong").get_text()
 
-following = soup.findAll("a", {"data-element-term":"following_stats"})
-print "Following: " + following[0].find("strong").get_text()
+def	scrapFollow():
+	r = requests.get("http://twitter.com/" + sys.argv[1])
+	soup = BeautifulSoup(r.text)
+
+	numTweets = soup.findAll("a", {"data-element-term":"tweet_stats"})
+	tweets = numTweets[0].find("strong")	
+
+	numFollowers = soup.findAll("a", {"data-element-term":"follower_stats"})
+	followers = numFollowers[0].find("strong").extract()
+
+	numFollowing = soup.findAll("a", {"data-element-term":"following_stats"})
+	following = numFollowing[0].find("strong")
+
+	time = datetime.now().strftime('%H:%M')
+
+	c = csv.writer(open("SOTU-STATS.csv", "a"))
+	c.writerow([time, sys.argv[1], followers, following, tweets])
+
+scrapFollow()
